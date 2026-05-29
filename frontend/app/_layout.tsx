f4cee368809315ebe76b7edd0d11b6fe1b711ad3
Bigperm1 <jwellsmorton@gmail.com>
2026-05-29 16:43:15 -0700

    Stability: start keepAlive on app launch

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -2,8 +2,12 @@ import React from "react";
 import { Stack } from "expo-router";
 import { AuthProvider } from "../src/auth";
 import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { startKeepAlive } from "../src/keepAlive";
+  
 
 export default function RootLayout() {
+    useEffect(() => { startKeepAlive(); }, []);
   return (
     <AuthProvider>
       <StatusBar style="light" />
